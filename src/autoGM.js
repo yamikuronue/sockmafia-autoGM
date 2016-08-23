76fba194ec3c9077a425611452312edc0e1a6a25
Yami <yamikuronue@gmail.com>
2016-08-23 13:37:34 +0000

    Chore: Basic structure of plugin

---
+++ src/autoGM.js
@@ -0,0 +1,58 @@
+const debug = require('debug')('sockbot:mafia');
+
+/**
+ * Sockbot 3.0 Activation function
+ * @returns {Promise} A promise that will resolve when the activation is complete
+ */
+exports.activate = function activate() {
+    
+}
+
+
+/**
+ * Sockbot 3.0 Plugin function
+ * @param  {Forum} forum  The forum provider's Forum class
+ * @param  {Object} config The plugin-specific configuration
+ * @returns {Object}        A temporary object representing this instance of the forum
+ */
+exports.plugin = function plugin(forum, config) {
+    
+}
+
+exports.setTimer = function(expires, label) {
+    
+}
+
+
+exports.init = function() {
+    
+}
+
+exports.startGame = function() {
+    
+}
+
+
+exports.onNightEnd = function() {
+    
+}
+
+exports.onDayEnd = function() {
+    
+}
+
+exports.onLynch = function() {
+    
+}
+
+exports.onNightEnd = function() {
+    
+}
+
+exports.checkWin = function() {
+    
+}
+
+exports.endGame = function() {
+    
+}
